b9e48c785ba6c597bab094a6d36064e9d5511a3c
Davin <65677163+davin2020@users.noreply.github.com>
2020-11-19 14:27:26 +0000

    Initialize tweaks

---
+++ app/routes.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->get('/', function ($request, $response, $args) use ($container) {
+        $renderer = $container->get('renderer');
+        return $renderer->render($response, "homepage.phtml");
+    });
+
+
+    $app->get('/{name}', function ($request, $response, $args) use ($container) {
+        $renderer = $container->get('renderer');
+        return $renderer->render($response, "homepage.phtml", $args);
+    });
+
+};
